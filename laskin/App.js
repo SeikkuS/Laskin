b2b2d8b55adfefb34717298bc4c066040681b826
SeikkuS <sahinseyhan2@gmail.com>
2023-01-29 18:29:38 +0200

    final commit

--- laskin/App.js
+++ laskin/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
-
+const App = () => {
   const [input1, setInput1] = useState(0);
   const [input2, setInput2] = useState(0);
 
   sum = () => {
-    result = input1 + input2;
+    result = parseFloat(input1) + parseFloat(input2);
     alert(result);
   }
 
-  subtract = () => {
-    result = input1 - input2;
-    alert(result);
-  }
+  
+subtract = () => {
+  result = parseFloat(input1) - parseFloat(input2);
+  alert(result);
+}
   
 
   return (
@@ -39,15 +37,15 @@ export default function App() {
         onChangeText = {text => setInput2(text)}
       />
       <View style = {{flexDirection: "row" }}>
-        <Button variant="outlined" title=" + " onPress={this.sum}></Button>
+        <Button variant="outlined" title=" + " onPress={sum}></Button>
         <Text>      </Text>
-        <Button variant="outlined" title=" - " onPress={this.subtract}></Button>
+        <Button variant="outlined" title=" - " onPress={subtract}></Button>
       </View>
         
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -57,3 +55,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
